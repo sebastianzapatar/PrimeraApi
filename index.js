7fe725fa65f70e6254cc300a5f96c3a0542d2c53
Sebastian Zapata <pansezapata@gmail.com>
2021-11-08 19:27:51 -0500

    Se cambia el puerto para heroku

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 const mongoose = require('mongoose');
 const app=require('./app');
-const port=2023;
+const port=8080;
 mongoose.connect('mongodb+srv://elcasique:TGXWeVbd1e97mO1q@cluster0.mwffh.mongodb.net/test', 
 {useNewUrlParser: true, 
 useUnifiedTopology: true}).then(()=>{
     console.log("Conecto");
-    app.listen(port,()=>{
+    app.listen(port,'0.0.0.0',()=>{
         console.log('Servidor corriendo en http://localhost:'+port)
     });
 })
