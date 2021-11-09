d00fdcb3a25b541d25eea51216bbef30b4de0559
Sebastian Zapata <pansezapata@gmail.com>
2021-11-08 19:44:36 -0500

    Se habiltan las opciones de los cors

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ const cors = require('cors');
 const app=express();
 //Cargas los archivos y asignarles una ruta
 const EstudianteUrl=require('./urls/EstudianteUrl');
+const corsOptions = {
+    origin: '*', // Aqui debemos reemplazar el * por el dominio de nuestro front
+    optionsSuccessStatus: 200 // Es necesario para navegadores antiguos o algunos SmartTVs
+}
+
 //middleware lo que traduce la peticion a json
 app.use(express.json());
 app.use(express.urlencoded());
+
 //Cors permite que otras aplicaciones se conecten
-app.use(cors());
+app.use(cors(corsOptions));
 //Añadir los prefijos
 app.use('/api',EstudianteUrl);
 //Exportar los modulos
